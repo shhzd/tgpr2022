573f305c1f1ad94f3c7511ffadc4afb08bb677c1
Younnes Chebli <younnesc@gmail.com>
2021-10-20 21:09:49 +0200

    feat(UC_Signup): ADD starting Signup

---
+++ src/tgpr/moudeule/controller/SignupController.java
@@ -0,0 +1,11 @@
+package tgpr.moudeule.controller;
+
+import tgpr.moudeule.MoudeuleApp;
+import tgpr.moudeule.model.User;
+import tgpr.moudeule.view.SignupView;
+import tgpr.moudeule.view.View;
+
+public class SignupController extends Controller {
+    //à continuer
+
+}
---
+++ src/tgpr/moudeule/view/SignupView.java
@@ -0,0 +1,11 @@
+package tgpr.moudeule.view;
+
+public class SignupView extends View{
+
+    public void displayHeader() {
+        clear();
+        println("\n=== Signup ===\n");
+    }
+
+    //à continuer
+}
