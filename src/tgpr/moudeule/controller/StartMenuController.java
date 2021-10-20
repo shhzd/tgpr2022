49e6b3e6e4cffed84ca2d9f604de2950af1a13b3
Silvere Sayag <silvere.sayag@gmail.com>
2021-10-20 23:04:07 +0200

    feat(UC_StartMenu): add StartMenuController.java

---
+++ src/tgpr/moudeule/controller/StartMenuController.java
@@ -0,0 +1,32 @@
+package tgpr.moudeule.controller;
+
+import tgpr.moudeule.view.StartMenuView;
+import tgpr.moudeule.view.View;
+
+public class StartMenuController extends Controller {
+    public void run() {
+        StartMenuView view = new StartMenuView();
+        try {
+            View.Action res;
+            do {
+//                view.displayHeaderWithLogo();
+
+                view.displayHeaderWithLogo();
+                view.displayMenu();
+                res = view.askForAction();
+                switch (res.getAction()) {
+                    case 'C':
+                        new LoginController().run();
+                        break;
+                    case 'E':
+                        new SignUpController().run();
+                        break;
+                }
+            } while (res.getAction() != 'Q');
+        } catch (View.ActionInterruptedException e) {
+            // just leave
+        }
+        view.pausedWarning("A bientôt.");
+        view.close();
+    }
+}
