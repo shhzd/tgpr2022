3066854b3890a45197767fc9a5960de592a14625
BERISHA AGIM <2704agberisha@domepfc.net>
2021-10-20 21:24:02 +0200

    feat(UC_Login): UPDATE LoginController Redirect MainMenu

--- src/tgpr/moudeule/controller/LoginController.java
+++ src/tgpr/moudeule/controller/LoginController.java
@@ -38,7 +38,11 @@ public class LoginController extends Controller {
             var user = askPseudo();
             askPassword(user);
             MoudeuleApp.setLoggedUser(user);
-            //new MainMenuController().run();
+            if(user.role.getRoleId() == 1) {
+                new StudentMainMenuController().run();
+            } else {
+                new TeacherMainMenuController().run();
+            }
         } catch (View.ActionInterruptedException e) {
             view.pausedWarning("aborted login");
         }
