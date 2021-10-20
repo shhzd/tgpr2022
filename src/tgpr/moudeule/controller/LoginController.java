155647a91a73fd3b280a7ff5e9daf080f3df89af
BERISHA AGIM <2704agberisha@domepfc.net>
2021-10-20 20:51:23 +0200

    feat(UC_Login): ADD LoginController & LoginView

---
+++ src/tgpr/moudeule/controller/LoginController.java
@@ -0,0 +1,46 @@
+package tgpr.tuto.controller;
+
+import tgpr.moudeule.model.User;
+import tgpr.moudeule.MoudeuleApp;
+import tgpr.moudeule.controller.Controller;
+import tgpr.moudeule.view.LoginView;
+import tgpr.moudeule.view.View;
+
+public class LoginController extends Controller {
+
+    private final LoginView view = new LoginView();
+
+    private User askPseudo() {
+        String pseudo;
+        User user;
+        do {
+            pseudo = view.askPseudo();
+            user = User.getByPseudo(pseudo);
+            if (user == null)
+                view.error("unknown user");
+        } while (user == null);
+        return user;
+    }
+
+    private String askPassword(User user) {
+        String password = null;
+        do {
+            password = view.askPassword();
+            if (!password.equals(user.getPassword()))
+                view.error("bad password");
+        } while (!password.equals(user.getPassword()));
+        return password;
+    }
+
+    public void run() {
+        view.displayHeader();
+        try {
+            var user = askPseudo();
+            askPassword(user);
+            MoudeuleApp.setLoggedUser(user);
+            //new MainMenuController().run();
+        } catch (View.ActionInterruptedException e) {
+            view.pausedWarning("aborted login");
+        }
+    }
+}
---
+++ src/tgpr/moudeule/view/LoginView.java
@@ -0,0 +1,16 @@
+package tgpr.moudeule.view;
+
+public class LoginView extends View {
+
+    public void displayHeader() {
+        displayHeader("Login");
+    }
+
+    public String askPseudo() {
+        return askString("Pseudo: ", null);
+    }
+
+    public String askPassword() {
+        return askString("Password: ", null, true);
+    }
+}
