5bcb80f53dc73c01c031d32004add0e2be0367f9
Younnes Chebli <younnesc@gmail.com>
2021-10-24 01:31:24 +0200

    feat(UC_Signup): ADD signup files

--- src/tgpr/moudeule/view/SignupView.java
+++ src/tgpr/moudeule/view/SignupView.java
@@ -1,11 +1,39 @@
 package tgpr.moudeule.view;
 
+import tgpr.moudeule.model.User;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+
 public class SignupView extends View{
 
     public void displayHeader() {
-        clear();
-        println("\n=== Signup ===\n");
+        super.displayHeader("S'enregistrer");
     }
 
-    //à continuer
+    public String askPseudo(String actual) {
+        return askString("Pseudo (" + actual + "): ", actual);
+    }
+
+    public String askPassword(String actual) {
+        String stars = null;
+        if (actual != null)
+            stars = String.join("", Collections.nCopies(actual.length(), "*"));
+        return askString("Mot de passe (" + stars + "): ", actual, true);
+    }
+
+    public String askFullname(String actual) {
+        return askString("Nom complet (" + actual + "): ", actual);
+    }
+
+    public LocalDate askBirthDate(LocalDate actual) {
+        return askDate("Date de naissance (" +
+                (actual == null ? "null" : DateTimeFormatter.ofPattern("dd/MM/yyyy").format(actual)) + "): ", actual);
+    }
+
+    public View.Action askForAction() {
+        return doAskForAction(-1, "\n[V] Valider, [A] Annuler", "[vVaA]");
+    }
+
 }
