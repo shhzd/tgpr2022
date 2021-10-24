307ad000963d6c758f4d34f6fe0b57b3f26f54c7
Silvere Sayag <silvere.sayag@gmail.com>
2021-10-24 21:55:27 +0200

    feat(UC_TeacherMainMenu): UPDATE teacher main

---
+++ src/tgpr/moudeule/view/TeacherMainMenuView.java
@@ -0,0 +1,31 @@
+package tgpr.moudeule.view;
+
+import tgpr.moudeule.model.Course;
+
+import java.util.List;
+
+public class TeacherMainMenuView extends View {
+    public void displayHeader() {
+        displayHeader("Main Menu");
+    }
+
+    public void displayMenu(List<Course> courses, int page, int nbPages, int lgPage) {
+        println("== Liste des cours ==\n");
+
+        println(" ID   CODE Cap  Description");
+
+        for (int i = 1 ; (i + (page * lgPage)) < courses.size() && i < (lgPage + 1) ; i++)
+            println(courses.get((i - 1)+ (page * lgPage)).prettyPrint());
+
+        println("\n[ID] Selectionner un cours - " +
+                "[A] ajouter un nouveau cours " +
+                (((page + 1) != nbPages) ? "- [S]: page suivante " :
+                (page > 0) ? "- [P]: page précédente " : "") +
+                "\n[O] Logout"); //-
+    }
+
+    public String askForString() {
+        return askString("", "", false);
+    }
+
+}
