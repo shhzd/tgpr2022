d84d658a3d69c794f176737a34a6450c54692e3e
Younnes Chebli <younnesc@gmail.com>
2021-10-27 21:42:11 +0200

    (feat(TeacherEditCourse) : ADD TeacherEditCourse files

---
+++ src/tgpr/moudeule/view/TeacherEditCourseView.java
@@ -0,0 +1,10 @@
+package tgpr.moudeule.view;
+
+
+
+public class TeacherEditCourseView extends View {
+    public void displayHeader() {
+        super.displayHeader("Modificatoin du cours de ");
+    }
+
+}
