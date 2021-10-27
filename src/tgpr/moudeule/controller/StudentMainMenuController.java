3155a05629f052344c8e58ae9d84c9e6f9a1d00c
Agim <agimm1@hotmail.com>
2021-10-27 21:14:50 +0200

    feat(UC_StudentMainMenu): ADD Controller and View

---
+++ src/tgpr/moudeule/controller/StudentMainMenuController.java
@@ -0,0 +1,9 @@
+package tgpr.moudeule.controller;
+
+
+public class StudentMainMenuController extends Controller {
+   StudentMainMenuController view = new StudentMainMenuController();
+    public void run() {
+        view.displayHeader();
+    }
+}
---
+++ src/tgpr/moudeule/view/StudentMainMenuView.java
@@ -0,0 +1,11 @@
+package tgpr.moudeule.view;
+
+import tgpr.moudeule.model.Course;
+import java.util.List;
+
+public class StudentMainMenuView extends View{
+    public void displayHeader(){
+        displayHeader("MainMenu");
+    }
+}
+
