6246346aa03caa9024f2915c2e8c2531a66fe813
Shahzad Rahman <shhzd.rhmn@gmail.com>
2021-10-27 20:17:42 +0200

    feat(UC_TeacherQuizzesList) : added displayheader

--- src/tgpr/moudeule/view/TeacherQuizzesListView.java
+++ src/tgpr/moudeule/view/TeacherQuizzesListView.java
@@ -4,4 +4,7 @@ package tgpr.moudeule.view;
 
 public class TeacherQuizzesListView extends View{
 
+    public void displayheader(){
+        super.displayHeader("Liste des quiz du cours");
+    }
 }
