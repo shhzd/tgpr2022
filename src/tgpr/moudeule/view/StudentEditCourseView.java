597ba026e3fe7004319f9870392ac63516db9bca
Younnes Chebli <younnesc@gmail.com>
2021-11-10 19:26:15 +0100

    build: DELETE files temporarily for the merge into develop

--- src/tgpr/moudeule/view/StudentEditCourseView.java
+++
@@ -1,9 +0,0 @@
-package tgpr.moudeule.view;
-
-public class StudentEditCourseView extends View{
-
-    public void displayHeader() {
-        super.displayHeader("Modificatoin du cours de ");
-    }
-
-}
