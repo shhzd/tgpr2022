e228e92f37206672b3a2b4414cf492fda545c1c1
Younnes Chebli <younnesc@gmail.com>
2021-11-11 21:36:43 +0100

    build(TeacherEditCourse): DELETE files temporarly :q

--- src/tgpr/moudeule/view/TeacherEditCourseView.java
+++
@@ -1,9 +0,0 @@
-package tgpr.moudeule.view;
-
-public class TeacherEditCourseView extends View {
-
-    public void displayHeader() {
-        super.displayHeader("Modificatoin du cours de ");
-    }
-
-}
