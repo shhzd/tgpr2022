51afeb7f8f94f795db6f35db9ae7f9e9070c3190
silvere.sayag@gmail.com <silvere.sayag@gmail.com>
2021-11-11 17:05:10 +0100

    fix(UC_TeacherStartMenu) Fixed display page bug

--- src/tgpr/moudeule/view/TeacherMainMenuView.java
+++ src/tgpr/moudeule/view/TeacherMainMenuView.java
@@ -11,20 +11,18 @@ public class TeacherMainMenuView extends View {
     }
 
     public void displayMenu(List<Course> courses, int page, int nbPages, int lgPage, User user) {
-        println("== Liste des cours ==\n");
+        println("== Liste des cours - page " + page + " de " + nbPages + " ==\n");
 
         println(" ID   CODE Cap  Description");
 
-        for (int i = 1 ; (i + (page * lgPage)) <= courses.size() && i < (lgPage + 1) ; i++) {
-            Course course = courses.get((i - 1) + (page * lgPage));
-            if (course.getTeacher().equals(user)) {
-                println(course.prettyString());
-            }
+        for (int i = 0 ; (i + (page * lgPage)) <= courses.size() && i < (lgPage + 1) ; i++) {
+            Course course = courses.get(i + ((page - 1) * lgPage));
+            println(course.prettyString());
         }
         println("   0  Ajouter un nouveau cours");
         println("\n[ID] Selectionner un cours " +
-                (((page + 1) != nbPages) ? "- [S] page suivante " :
-                (page > 0) ? "- [P] page précédente " : "") +
+                ((page != nbPages) ? "- [S] page suivante " : "") +
+                ((page > 1) ? "- [P] page précédente " : "") +
                 "\n[Q] Quitter Moudeule"); //-
     }
 
