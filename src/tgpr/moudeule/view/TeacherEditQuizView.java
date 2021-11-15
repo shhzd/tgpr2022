1e75fc49d9480d741d791d587c97e6534b0c2e9e
Linh P. Nguyen <nguyenlp.info@gmail.com>
2021-11-15 22:28:13 +0100

    feat(UC_TeacherEditQuiz): ADD displayTitle method in View

--- src/tgpr/moudeule/view/TeacherEditQuizView.java
+++ src/tgpr/moudeule/view/TeacherEditQuizView.java
@@ -1,11 +1,16 @@
 package tgpr.moudeule.view;
 
 import tgpr.moudeule.model.Course;
+import tgpr.moudeule.model.Quiz;
 
 public class TeacherEditQuizView extends View {
 
     public void displayHeader(Course course) {
         super.displayHeader("Modifier un quiz - cours " + course.getId());
+    }
+
+    public void displayTitle(Quiz quiz) {
+        println("== " + quiz.getTitle() + " ==");
     }
 
 }
