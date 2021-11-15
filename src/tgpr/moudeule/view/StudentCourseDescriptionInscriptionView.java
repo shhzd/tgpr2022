bb0afe0c866eabe4903cf203511e79a525e2dbc2
Linh P. Nguyen <nguyenlp.info@gmail.com>
2021-11-15 12:59:55 +0100

    feat(UC_StudentCourseDescriptionInscription): ADDING displayHeader method in View

--- src/tgpr/moudeule/view/StudentCourseDescriptionInscriptionView.java
+++ src/tgpr/moudeule/view/StudentCourseDescriptionInscriptionView.java
@@ -2,4 +2,7 @@ package tgpr.moudeule.view;
 
 public class StudentCourseDescriptionInscriptionView extends View {
 
+    public void displayHeader() {
+        super.displayHeader("Détail d'un cours");
+    }
 }
