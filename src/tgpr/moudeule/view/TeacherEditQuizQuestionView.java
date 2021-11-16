04539df4829f0f5375933df6bbd4e3c2bc7732c0
Linh P. Nguyen <nguyenlp.info@gmail.com>
2021-11-16 15:46:08 +0100

    feat(UC_TeacherEditQuizQuestion): ADD displayHeader in view

--- src/tgpr/moudeule/view/TeacherEditQuizQuestionView.java
+++ src/tgpr/moudeule/view/TeacherEditQuizQuestionView.java
@@ -1,4 +1,10 @@
 package tgpr.moudeule.view;
 
+import tgpr.moudeule.model.Question;
+
 public class TeacherEditQuizQuestionView extends View {
+
+    public void displayHeader(Question question){
+        super.displayHeader("Modifier une question - Cours " + question.getCourseCodeByQuestionId());
+    }
 }
