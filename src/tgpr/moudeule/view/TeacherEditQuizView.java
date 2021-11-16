55c4810b591d5f3b3ae58f9b1498c5851dbf1734
Linh P. Nguyen <nguyenlp.info@gmail.com>
2021-11-16 02:16:04 +0100

    feat(UC_TeacherEditQuiz): UPDATE display questions fixing display issue

--- src/tgpr/moudeule/view/TeacherEditQuizView.java
+++ src/tgpr/moudeule/view/TeacherEditQuizView.java
@@ -2,6 +2,7 @@ package tgpr.moudeule.view;
 
 import tgpr.moudeule.model.Question;
 import tgpr.moudeule.model.Quiz;
+import tgpr.moudeule.model.VariableForTesting;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -10,7 +11,7 @@ import java.util.List;
 public class TeacherEditQuizView extends View {
 
     public void displayHeader(Quiz quiz) {
-        super.displayHeader("Modifier un quiz - cours " + quiz.getId());
+        super.displayHeader("Modifier un quiz - cours " + quiz.getCourseCode());
     }
 
     public void displayTitle(Quiz quiz) {
@@ -20,18 +21,23 @@ public class TeacherEditQuizView extends View {
     public void displayOption(Quiz quiz, int page, int nbPages, int lgPage) {
         println("[ESC] Pour abandonner la modification");
         println("\n[1] Modifier le titre : " + quiz.getTitle());
-        println("[2] Modifier la date de début : " + quiz.getClass());
+        println("Current date " + VariableForTesting.getCurrentDate());
+        println("[2] Modifier la date de début : " + quiz.getStart());
         println("[3] Modifier la date de fin : " + quiz.getFinish());
+        println("\n");
         List<Question> questions = Question.getQuestionsByQuiz(quiz.getId());
         if(questions.size() > 0) {
-            int index = 4 + (page * lgPage);
+            int index = 4 + ((page - 1) * lgPage);
+            int k = 0;
             for(int j = (page - 1) * lgPage; j < questions.size(); ++j) {
-                if(j < questions.size() && j < lgPage) {
-                    println("[" + index + "] la question " + (index - 3) * (page * lgPage) + " : " + questions.get(j).getTitle());
+                if(j < questions.size() && k < lgPage) {
+                    println("[" + index + "] la question " + (index - 3) + " : " + questions.get(j).getTitle());
+                    ++index;
+                    ++k;
                 }
             }
         }
-        println("\n[R] Retour - [Q] Quitter - " + (((page) != nbPages) ? " - [S] Page suivante " : "") +
+        println("\n[R] Retour - [Q] Quitter" + ((page != nbPages) ? " - [S] Page suivante " : "") +
                 ((page > 1) ? " - [P] Page précédente " : ""));
     }
 
