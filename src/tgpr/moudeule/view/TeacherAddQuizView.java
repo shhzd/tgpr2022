db01a38f839552a2deb95c7504510b63d7b24106
shhzd <shhzd.rhmn@gmail.com>
2021-11-19 00:51:04 +0100

    feat(UC_TeacherAddQuiz):added checks for dates

--- src/tgpr/moudeule/view/TeacherAddQuizView.java
+++ src/tgpr/moudeule/view/TeacherAddQuizView.java
@@ -1,29 +1,69 @@
 package tgpr.moudeule.view;
 
 import tgpr.moudeule.model.Course;
+import tgpr.moudeule.model.Quiz;
+
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 
 public class TeacherAddQuizView extends View{
-    private final Course course;
 
-    public TeacherAddQuizView(Course course){
-        this.course = course;
-    }
-
-    public void displayHeader(){
-        super.displayHeader(" Créez un nouveau quiz ");
+    public void displayHeader(Course course){
+        super.displayHeader(" Ajouter un quiz - cours " + course.getCode());
     }
 
     public String askTitle(String actual){
-        return askString("Veuillez entrer un titre pour le quiz : ",actual);
+        return askString("Ajoutez un titre : ",actual);
     }
 
     public LocalDate askStartDate(LocalDate actual){
-        return askDate("Veuillez entrer une date de début pour le quiz : ", actual);
+        return askDate("Ajoutez la date de début : "+
+                (actual == null ? "jj/mm/aaaa" : DateTimeFormatter.ofPattern("dd/MM/yyyy").format(actual)) + "): ", actual);
+    }
+
+    public void displayStartDateError(Quiz quiz, LocalDate newDate) {
+        if(!quiz.newStartDateIsAfterCurrentDate(newDate)) {
+            showError("La date de début ne peut être dans le passé !");
+        }
     }
 
     public LocalDate askFinishDate(LocalDate actual){
-        return askDate("Veuillez entrer une date de fin pour le quiz : ", actual);
+        return askDate("Ajoutez la date de fin : " +
+                (actual == null ? "jj/mm/aaaa" : DateTimeFormatter.ofPattern("dd/MM/yyyy").format(actual)) + "): ", actual);
+    }
+
+    public void displayFinishDateError(Quiz quiz, LocalDate newDate) {
+        if(!quiz.newFinishedDateisAfterCurrentStartDate(newDate)) {
+            showError("La date de fin ne peut pas être avant la date de début !");
+        }
+    }
+
+    public String askQuestion(String actual){
+        return askString("Ajoutez l'énoncé de la question : ", actual);
+    }
+
+    public String askType(String actual){
+        return askString("Est-ce une [1] QCM ou une [2] QRM ?", actual);
+    }
+
+    public void askAddOption(){
+        println("[O/N] Voulez-vous ajouter une proposition ? ");
+    }
+
+    public String enterOptionText(String actual){
+        return askString("Entrez la proposition : ", actual);
+    }
+
+    public String enterOptionValue(String actual){
+        return askString("Cette proposition est-elle [1] vraie ou [2] fausse ? ", actual);
+    }
+
+    public void addNewQuestion(){
+        println("[O/N] Voulez vous ajouter une question ? ");
+    }
+
+    public String askForString() {
+        return askString("", "", false);
     }
 }
