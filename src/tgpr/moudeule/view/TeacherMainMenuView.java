d9abcc53b5e0b11830c0f2bb8a0fe683e75eb961
silvere.sayag@gmail.com <silvere.sayag@gmail.com>
2021-11-19 16:14:41 +0100

    fix(UC_TeacherMainMenu) Selection and display bugs squashed

--- src/tgpr/moudeule/view/TeacherMainMenuView.java
+++ src/tgpr/moudeule/view/TeacherMainMenuView.java
@@ -10,22 +10,32 @@ public class TeacherMainMenuView extends View {
         displayHeaderWithPseudo("Main Menu");
     }
 
-    public void displayMenu(List<Course> courses, int page, int nbPages, int lgPage) {
-        println("== Liste des cours - page " + page + " de " + nbPages + " ==\n");
-
-        println(" ID   CODE Cap  Description");
-
-        for (int i = 0 ; (i + ((page - 1) * lgPage)) < courses.size() && i < (lgPage + 1) ; i++) {
-            Course course = courses.get(i + ((page - 1) * lgPage));
-            println(course.prettyString());
-        }
-        println("   0  Ajouter un nouveau cours");
-        println("\n[ID] Selectionner un cours " +
-                ((page != nbPages) ? "- [S] page suivante " : "") +
-                ((page > 1) ? "- [P] page précédente " : "") +
-                "\n[Q] Quitter Moudeule"); //-
+    public void displaySubHeader(int page, int nbPages) {
+        println("== Liste des cours - page " + page +
+                ((nbPages > 1) ? " de " + nbPages : "") +
+                " ==\n");
     }
 
+    public void displayMenu(List<Course> courses, int page, int nbPages, int lgPage) {
+
+        if (courses.size() != 0) {
+            println(" ID   CODE Cap  Description");
+            for (int i = 0; (i + ((page - 1) * lgPage)) < courses.size() && i < (lgPage + 1); i++) {
+                Course course = courses.get(i + ((page - 1) * lgPage));
+                println(course.prettyString());
+            }
+        } else {
+            println("Vous n'avez pas encore de cours.");
+        }
+    }
+
+    public void displayNavigationMenu(int page, int nbPages) {
+        println("\n[ID] Selectionner un cours - [0]  Ajouter un nouveau cours");
+        println("[Q] Quitter Moudeule" +
+                ((page != nbPages) ? "- [S] page suivante " : "") +
+                ((page > 1) ? "- [P] page précédente " : "")
+        );
+    }
     public String askForString() {
         return askString("", "", false);
     }
