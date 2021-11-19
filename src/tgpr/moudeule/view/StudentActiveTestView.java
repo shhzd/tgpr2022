1c67f15533c5cd3fd1e92447553a38a86c616a17
Linh P. Nguyen <nguyenlp.info@gmail.com>
2021-11-19 21:22:50 +0100

    feat(StudentActiveTest): ADD display methods in view

--- src/tgpr/moudeule/view/StudentActiveTestView.java
+++ src/tgpr/moudeule/view/StudentActiveTestView.java
@@ -1,4 +1,32 @@
 package tgpr.moudeule.view;
 
+import tgpr.moudeule.model.Option;
+import tgpr.moudeule.model.Question;
+import tgpr.moudeule.model.Test;
+
+import java.util.*;
+
 public class StudentActiveTestView extends View {
+
+    void displayHeader(Test test) {
+        super.displayHeader("test " + test);
+    }
+
+    void displayQuestion(int order) {
+        println("== QUESTION " + order + " ==");
+    }
+
+    void displayStatement(Question question) {
+        println(question.getTitle());
+    }
+
+    void shuffleOptions(List<Option> options) {
+
+    }
+
+    void displayPropositions(List<Option> options) {
+        Random r = new Random();
+        Collections.shuffle(options);
+    }
+
 }
