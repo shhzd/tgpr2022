4f2b1358501d12ff9400a1565ead60be5b1cee05
silvere.sayag@gmail.com <silvere.sayag@gmail.com>
2021-11-23 15:48:36 +0100

    feat(Controller) added gloabl NUMBER_DISPLAY_LINE

--- src/tgpr/moudeule/controller/Controller.java
+++ src/tgpr/moudeule/controller/Controller.java
@@ -1,6 +1,9 @@
 package tgpr.moudeule.controller;
 
 public abstract class Controller {
+
+    static final int NUMBER_DISPLAY_LINE = 14;
+
     public abstract void run();
 
     public static boolean isParsable(String input) {
